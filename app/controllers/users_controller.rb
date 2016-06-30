5a93a811445c7d98a00ec24c550cf885bda3e357
witty98 <witty98@outlook.com>
2016-06-30 11:00:27 +0800

    完成登录/登出功能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user      
       flash[:success] = "欢迎注册!"
       redirect_to @user
     else
